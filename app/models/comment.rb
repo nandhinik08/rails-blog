194c23943bf17ccd1e8378fb8580364dbb63498d
Nandhini <nandhinimu27@gmail.com>
2021-11-14 20:58:55 +0530

    option to delete comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,3 +2,22 @@ class Comment < ApplicationRecord
   include Visible
   belongs_to :article
 end
+class CommentsController < ApplicationController
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article)
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
+end
